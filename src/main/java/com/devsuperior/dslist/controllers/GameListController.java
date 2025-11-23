5274fe383f9758be501781012cba1c182ea981da
renanferri <renan.gferri@gmail.com>
2025-11-23 18:03:06 -0300

    feature move order game at the list

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.dto.ReplecementDto;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 
@@ -31,5 +34,10 @@ public class GameListController {
 	@GetMapping(value = "/{listId}/games")
 	public List<GameMinDto> findByList(@PathVariable Long listId) {
 		return gameService.findByList(listId);		
+	}
+	
+	@PostMapping(value = "/{listId}/games")
+	public void move(@PathVariable Long listId, @RequestBody ReplecementDto body) {
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());		
 	}
 }
